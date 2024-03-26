eb8a86d3ca6ad2ae0252c075d42dcaeb620a9197
Nguyễn Kim Dũng <kimdungvn52@gmail.com>
2024-03-26 10:14:03 +0700

    image upload function

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import { Alert, Button, TextInput } from 'flowbite-react'
+import React, { useState, useRef, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function DashProfile() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  }
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError('Could not upload image (File must be less than 2MB)');
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        })
+      }
+    )
+  }
+
   return (
-    <div>
-      profile
+    <div className='max-w-lg mx-auto p-3 w-full'>
+      <h1 className='my-7 text-center font-semibold text-3xl' style={{ color: 'lightcoral' }}>Profile</h1>
+      <form className='flex flex-col gap-5'>
+        <input type='file' accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden />
+        <div className=" relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
+          {imageFileUploadProgress && (
+            <CircularProgressbar
+              value={imageFileUploadProgress || 0}
+              text={`${imageFileUploadProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: '100%',
+                  height: '100%',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(240, 128, 128, ${imageFileUploadProgress / 100})`,
+                }
+              }} />
+          )}
+          <img
+            src={imageFileUrl || currentUser.profilePicture}
+            alt='user'
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightcoral] ${imageFileUploadProgress && imageFileUploadProgress < 100 && 'opacity-40'}`}
+          ></img>
+          {imageFileUploadError && <Alert color='failure'>{imageFileUploadError}</Alert>}
+        </div>
+        <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} />
+        <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} />
+        <TextInput type='password' id='password' placeholder='password' />
+        <Button type='submit' className='bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...'>Update</Button>
+      </form>
+      <div className="text-red-500 flex justify-between mt-3">
+        <span className='cursor-pointer transition duration-500 ease-in-out transform hover:text-yellow-500 hover:scale-110'>Delete Account</span>
+        <span className='cursor-pointer transition duration-500 ease-in-out transform hover:text-yellow-500 hover:scale-110'>Sign Out</span>
+
+      </div>
     </div>
   )
 }
