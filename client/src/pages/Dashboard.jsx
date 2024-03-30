f2e207e2dda499d3270543a39712f9825901e91b
Nguyễn Kim Dũng <kimdungvn52@gmail.com>
2024-03-30 23:25:49 +0700

    dashboard overview admin complete

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
   const location = useLocation(); // give the access to the current URL location in the browser
@@ -30,6 +31,8 @@ export default function Dashboard() {
       {tab === 'users' && <DashUsers />}
       {/* Comments */}
       {tab === 'comments' && <DashComments/>}
+      {/* Dashboard */}
+      {tab === 'dash' && <DashboardComp/>}
     </div>
   )
 }
